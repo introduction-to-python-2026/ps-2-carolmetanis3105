340047fcaa00948da053df529eb14f4bae1842c4
carolmetanis3105 <carol.metanis@gmail.com>
2025-10-30 12:10:26 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -7,9 +7,12 @@ def find_max_number(num1, num2, num3):
       return num3
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+  x = num1 + num2 + num3
+    avg = float(x/3)
+    return avg
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    z = (((num1 - mean)** 2 + (num2 - mean)** 2 + (num3 - mean)** 2)/3)**0.5
 
+    return mean, z
